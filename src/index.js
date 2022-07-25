1e222bb24c6221bc9df2baad45003a9995c4c9de
ssvetluna <s.stepanenko.a@gmail.com>
2022-07-25 20:56:53 +0300

    Added main icon

--- src/index.js
+++ src/index.js
@@ -28,13 +28,20 @@ function dispalayTemperature(response) {
   let windElement = document.querySelector("#currentwind");
   let descriptionElement = response.data.weather[0].description;
   let dateElement = document.querySelector("#currentdate");
+  let iconElement = document.querySelector(".main-emoji");
+  console.log(iconElement);
   temperaturaElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "2f7f11cce544f115af9a2c80b2a612b4";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&units=metric&appid=${apiKey}`;
+let city = "New York";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 axios.get(url).then(dispalayTemperature);

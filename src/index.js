899a62312f31048c4cf4ad660bc7a102405c16d2
ssvetluna <s.stepanenko.a@gmail.com>
2022-07-25 17:19:34 +0300

    Update the time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Tuesday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function dispalayTemperature(response) {
   let temperaturaElement = document.querySelector(".main-temp");
-  temperaturaElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#cityName");
-  cityElement.innerHTML = response.data.name;
   let humidityElement = document.querySelector("#currenthum");
-  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   let windElement = document.querySelector("#currentwind");
-  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
   let descriptionElement = response.data.weather[0].description;
+  let dateElement = document.querySelector("#currentdate");
+  temperaturaElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "2f7f11cce544f115af9a2c80b2a612b4";
